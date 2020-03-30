84083ebef68b3248a4989c061a5acbf1b18970b4
Miquel de Domingo i Giralt <62285815+miqueldedo@users.noreply.github.com>
2020-03-30 19:32:54 +0200

    Menu now handled form Chess.java

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -5,38 +5,30 @@
 import javafx.application.Application;
 
 /* 
+ * @file ChessGame.java
  * @class ChessGame
  * @brief Main class of the application. Handles whereas the app will be
  *        displayed in a UI or in the console depending on the given arguments
  */
 public abstract class ChessGame {
 	/*
-	 * @pre args is [-g] or [-c] and [gameRules.json]
+	 * @pre args is [-g] or [-c] 
 	 * @post Runs UI if -g or console if -c
 	 */
 	public static void main(String[] args) {
 		try {
-			/// Length == 2 -> mode + configuration file location
-			/// Length == 1 -> mode
-			if (args[0].equals("-g")) {
-				/// Run UI Application - Ask for files when screen opens.
-				Application.launch(UIChess.java, null);
-			} else if (args[0].equals("-c")) {
-				switch (args.length) {
-					case 1:
-						/// Run game with default configuration
-						ConsoleGame.play(ChessJSONParser.buildChess(/* DEFAULT FILE LOCATION */));
-						break;
-					case 2:
-						/// Build Chess with the given configuration
-						ConsoleGame.play(ChessJSONParser.buildChess(args[1]));
-						break;
-					default:
-						_displayError();
-						break;
+			if (args.length > 0) {
+				if (args[0].equals("-g")) {
+					/// Run UI Application - Ask for files when screen opens.
+					/// Application.launch(UIChess.java, null);
+					System.out.println("UI Application running.");
+				} else if (args[0].equals("-c")) {
+					ConsoleGame.start();
+				} else {
+					_displayHelp();
 				}
 			} else {
-				_displayError();
+				_displayHelp();
 			}
 		} catch (Exception e) {
 			/// Class constructors can return exception if file not found.
@@ -45,9 +37,14 @@ public abstract class ChessGame {
 		}
 	}
 
-	private static void _displayError() {
-		System.out.println("\nIncorrect arguments number.");
-		System.out.println(
-				"\nGame can be run with user interface [-g] or in the console [-c]. \nYour arguments were not valid.");
+	/*
+	 * @brief Mostra un missatge d'ajuda
+	 * @pre Nombre d'arguments entrar incorrecte
+	 * @post Mostra per pantalla el missatge d'ajuda sobre com executar l'aplicació.
+	 */
+	private static void _displayHelp() {
+		System.out.println("Nombre incorrecte d'arguments");
+		System.out.println("El joc es pot executar amb interfície gràfica [-g] o en consola [-c].");
+		System.out.println("Exemple: ChessGame -c");
 	}
 }
