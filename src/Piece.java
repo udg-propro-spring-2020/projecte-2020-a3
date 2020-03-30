7bec873fc0ebdaa38238f666eb0edf9f214786a6
Miquel de Domingo i Giralt <62285815+miqueldedo@users.noreply.github.com>
2020-03-30 19:41:21 +0200

    Updated class documentation

--- src/Piece.java
+++ src/Piece.java
@@ -1,17 +1,38 @@
-import java.util.ArrayList;
+/*
+ * @author Miquel de Domingo i Giralt
+ */
 
+import java.util.List;
+
+/*
+ * @file Piece.java
+ * @class Piece
+ * @brief Holds the information of the piece
+ */
 public class Piece {
-    private String name;
-    private String symbol;
-    private String wImage;
-    private String bImage;
-    private int value;
-    private boolean promotable;
-    private boolean invulnerable;
-    private ArrayList<Movement> movements;
-    private ArrayList<Movement> initialMovements;
+    private String name;                            ///< Piece name
+    private String symbol;                          ///< Piece symbol
+    private String wImage;                          ///< Piece white image location
+    private String bImage;                          ///< Piece black image location
+    private int value;                              ///< Piece in-game value
+    private boolean promotable;                     ///< Piece's capacity to be promotable
+    private boolean invulnerable;                   ///< Piece's capacity to be invulnerable
+    private List<Movement> movements;               ///< Piece's available movement options
+    private List<Movement> initialMovements;        ///< Piece's special movements when first move
 
-    Piece(String name, String symbol, String wImage, String bImage, int value, boolean promotable, boolean invulnerable, ArrayList<Movement> movements, ArrayList<Movement> initialMovements){
+    /**
+     * @brief Piece constructor
+     * @param name Piece's name
+     * @param symbol Piece's symbol
+     * @param wImage Piece's white image location
+     * @param bImage Piece's black image location
+     * @param value Piece's in-game value
+     * @param promotable Piece's capacity to be promotable
+     * @param invulnerable Piece's capacity to be invulnerable
+     * @param movements Piece's available movement options
+     * @param initialMovements Piece's special movements when first move
+     */
+    Piece(String name, String symbol, String wImage, String bImage, int value, boolean promotable, boolean invulnerable, List<Movement> movements, List<Movement> initialMovements){
         this.name = name;
         this.symbol = symbol;
         this.wImage = wImage;
@@ -21,6 +42,36 @@ public class Piece {
         this.invulnerable = invulnerable;
         this.movements = movements;
         this.initialMovements = initialMovements;
+    }
+
+    /**
+     * @brief Obtain the piece's symbol
+     * @pre --
+     * @return @c this.symbol 
+     * @post Returns the piece's symbol which can't be @c null
+     */
+    public String symbol(){
+        return symbol;
+    }
+    
+    /**
+     * @brief Obtain the piece's name
+     * @pre --
+     * @return @c this.name 
+     * @post Returns the piece's name which can't be @c null
+     */
+    public String name(){
+        return name;
+    }
+
+    /**
+     * @brief Obtain the piece's list of movements
+     * @pre --
+     * @return @c this.movements
+     * @post Returns the piece's list of movements which can't be @c null 
+     */
+    public List<Movement> movements(){
+        return movements;
     }
 
     @Override
@@ -56,15 +107,6 @@ public class Piece {
         }
         
         return s.toString();
-    }
-    public String symbol(){
-        return symbol;
-    }
-    public String name(){
-        return name;
-    }
-    public ArrayList<Movement> movements(){
-        return movements;
     }
 }
 
