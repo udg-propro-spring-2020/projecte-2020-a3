48067426a0920d6118504bcac22710f65fd83ce9
Miquel de Domingo i Giralt <62285815+miqueldedo@users.noreply.github.com>
2020-04-02 10:16:37 +0200

    Updated Movement class
    Added toJson method and documentation. Removed toString method since we can use toJson.

--- src/Movement.java
+++ src/Movement.java
@@ -1,41 +1,73 @@
-
-
 public class Movement {
+    /// @param X Fixed int or a 50 (-50) - meaning anything but 0 -
     private int x;
+    /// @param Y Fixed int or a 50 (-50) - meaning anything but 0 -
     private int y;
+    /// @param capture 0 (can't capture), 1 (can capture), 2 (movement valid only when capturing)
     private int capture;
-    private int jump;
+    /// @param jump Whether it can jump other pieces or not
+    private boolean jump;
 
+    /*
+     * @brief Default movement constructor
+     */
     Movement(int x, int y, int capture, int jump){
         this.x = x;
         this.y = y;
         this.capture = capture;
-        this.jump = jump;
+        this.jump = jump == 1 ? true : false;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append("[")
-         .append(x)
-         .append(", ")
-         .append(y)
-         .append(", ")
-         .append(capture)
-         .append(", ")
-         .append(jump)
-         .append("] \n");
-        
-        return s.toString();
-    }
+    /*
+     * @pre ---
+     * @post Returns the X property of the movement
+     */
     public int movX(){
         return x;
     }
+
+    /*
+     * @pre ---
+     * @post Returns the Y property of the movement
+     */
     public int movY(){
         return y;
     }
-    //captura: 0=no, 1=si, 2=mov possible nomes al matar
+    
+    /*
+     * @pre ---
+     * @post Returns the capture sign
+     */
     public int captureSign(){
         return capture;
+    }
+
+    /*
+     * @pre ---
+     * @post Returns a String of the object properties in JSON format (tabbed)
+     */
+    public String toJSON() {
+        StringBuilder s = new StringBuilder();
+        s.append("\t\t\t\t[\n")
+            .append("\t\t\t\t\t")                 /// Indentation
+            .append(
+                x == 50 ? "\"a\"" : 
+                    (x == -50 ? "\"-a\"" : Integer.toString(x)
+                )
+            ).append(", \n")
+            .append("\t\t\t\t\t")                 /// Indentation
+            .append(
+                y == 50 ? "\"a\"" : 
+                    (y == -50 ? "\"-a\"" : Integer.toString(y)
+                )
+            ).append(", \n")
+            .append("\t\t\t\t\t")                 /// Indentation
+            .append(Integer.toString(capture))
+            .append(", \n")
+            .append("\t\t\t\t\t")                 /// Indentation
+            .append(jump ? "1 \n" : "0 \n")
+            .append("\t\t\t\t]\n");
+        
+        return (s.toString());
     }
 }
