871acd03a5d0bf6fe1541fe3b4bae1e089b303a0
David Caceres <caceresdavid333@gmail.com>
2020-04-10 19:58:28 +0200

    Minor changes

--- src/Position.java
+++ src/Position.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author cacer
- */
 public class Position {
     public int row;
     public int col;
