46f4050990a4f3ba95042ad5aa9a4dbfa644d2aa
Joan Plaja Sanchez-Fortuny <joanplaja10@gmail.com>
2020-04-13 22:57:27 +0200

    MinMax actualitzar només falta la funcio de desfer un moviment per acabarlo

--- src/Cpu.java
+++ src/Cpu.java
@@ -19,7 +19,7 @@ public class Cpu{
     @post La cpu té el coneixament \p coneixament,una referencia al joc d'escacs \p escacs
     i una profunidtat de busqueda de moviments \p profunditat.
      */
-    public Cpu(Knowledge knowledge,Chess chess,int profunditat,string color){
+    public Cpu(Knowledge knowledge,Chess chess,int profunditat,int color){
         _knowledge=knowledge;
         _chess=chess;
         _profunditat=profunditat;
@@ -49,44 +49,64 @@ public class Cpu{
      */
     private Pair<Position,Position> minMax(){ 
         Pair<Position,Position> moviment = new Pair<Position,Position>(null,null);
-        return i_minMax(0,0,moviment);
+        return i_minMax(0,0,moviment,Integer.MIN_VALUE,Integer.MAX_VALUE);
 
     }
 
-    private int i_minMax(int puntuacio,int tipusJugador,Pair<Position,Position> moviment){
+    private int i_minMax(int puntuacio,int tipusJugador,Pair<Position,Position> moviment,int mesGranAnterior,int mesPetitAnterior){
         if(profunditat==_profunditat)return puntuacio;
         else if(tipusJugador==0){
             Integer max = Integer.MIN_VALUE;
-            while(){//per cada fitxa
-                while(){//per cada moviment
-                    Integer score= + puntuacio;
-                    //aplicar
+            List<Pair<Position,Piece>> pieces;
+            if(_color==0)pieces=_chess.pListWhite();
+            else pieces=_chess.pListBlack();
+            Iterator<Pair<Position,Piece>> itPieces = pieces.iterator();
+            while(itPieces.hasNext()){//per cada fitxa
+                List<Pair<Position,Integer>> destinyWithScores = _chess.destinyWithValues(itPieces.second);
+                Iterator<Pair<Position,Integer>> itMoviments;
+                while(itMoviments.hasNext()){//per cada moviment
+                    Pair<Position,Integer> moviment = itMoviments.next();
+                    Integer score=moviment.second + puntuacio;
+                    if(moviment.second>0)_chess.applyMovement(itPieces.second,null,moviment.first);//aplicar
+                    else _chess.applyMovement(itPieces.second,moviment.first,null);
                     score = minMax(score,profunditat+1,1,moviment);
-                    //desfer
+                    //desfer !!!
                     if(score>max){
+                        mesGranAnterior=score;
                         max=score;
-                        moviment.first=
-                        movimient.second=
+                        moviment.first=itPieces.second,
+                        movimient.second=moviment.second;
                     }
+                    if(mesPetitAnterior<=mesGranAnterior)break;
                 }
             }
             return max;
         }
         else{
             Integer min = Integer.MAX_VALUE;
-            while(){//per cada fitxa
-                while(){//per cada moviment
-                    Integer score= + puntuacio;
-                    //aplicar
+            List<Pair<Position,Piece>> pieces;
+            if(_color==1)pieces=_chess.pListWhite();
+            else pieces=_chess.pListBlack();
+            Iterator<Pair<Position,Piece>> itPieces = pieces.iterator();
+            while(itPieces.hasNext()){//per cada fitxa
+                List<Pair<Position,Integer>> destinyWithScores = _chess.destinyWithValues(itPieces.second);
+                Iterator<Pair<Position,Integer>> itMoviments;
+                while(itMoviments.hasNext()){//per cada moviment
+                    Pair<Position,Integer> moviment = itMoviments.next();
+                    Integer score= -moviment.second + puntuacio;
+                    if(moviment.second>0)_chess.applyMovement(itPieces.second,null,moviment.first);//aplicar
+                    else _chess.applyMovement(itPieces.second,moviment.first,null);
                     score = minMax(score,profunditat+1,0,moviment);
-                    //desfer
-                    if(score<min){
-                        min=score;
-                        moviment.first=
-                        movimient.second=
+                    //desfer !!!
+                    if(score>max){
+                        mesGranAnterior=score;
+                        max=score;
+                        moviment.first=itPieces.second,
+                        movimient.second=moviment.second;
+                    }
+                    if(mesGranAnterior>=mesPetitAnterior)break;
                     }
                 }
-            }
             return min;
         }
     }
