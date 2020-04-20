c22a06f9b877262b443bd8297614a6ba38c858ca
Miquel de Domingo <u1960520@campus.udg.edu>
2020-04-20 12:54:02 +0200

    Added JSON interface

---
+++ src/JSON.java
@@ -0,0 +1,12 @@
+/**
+ * @author Miquel de Domingo i Giralt
+ * @file JSON.java
+ * @class JSON
+ * @brief Interface for all classe which have to be converted to JSON
+ */
+public interface JSON {
+    /// @brief Parses the object properties to JSON style
+    /// @pre ---
+    /// @post Returns a tabbed string containing the object in JSON format
+    public String toJSON();
+}
