f9f3ba8f627d74dd64262fa2a50ceb2b5f5f44a0
Joan Plaja Sanchez-Fortuny <joanplaja10@gmail.com>
2020-05-28 11:11:57 +0200

    added documentation

--- src/Pair.java
+++ src/Pair.java
@@ -1,4 +1,6 @@
-/** @file Pair.java
+/** @author Joan Plaja
+    @file Pair.java
+    @class Pair
     @brief Un parell genèric
 */
 
@@ -9,6 +11,7 @@ public class Pair<S,T> implements Cloneable {
     public S first;
     public T second;
     
+    // @brief Create a pair of two given objects
 	public Pair(S first, T second) {
         this.first = first; 
         this.second = second;
@@ -23,9 +26,6 @@ public class Pair<S,T> implements Cloneable {
         } catch (CloneNotSupportedException c) {
             System.err.println("Pair clone exception");
         }
-        //cloned.first = (S) this.first.clone();
-        //cloned.second = (T) this.second.clone();
-
         return cloned;
     }
 
