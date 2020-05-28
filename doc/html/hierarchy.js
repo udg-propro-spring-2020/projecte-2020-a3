3e04e74355e508f63406f6cb399060c6edb61762
Miquel de Domingo i Giralt <62285815+miqueldedo@users.noreply.github.com>
2020-05-28 17:26:19 +0200

    Doxygen part 3

---
+++ doc/html/hierarchy.js
@@ -0,0 +1,44 @@
+var hierarchy =
+[
+    [ "ChessGame", "class_chess_game.html", null ],
+    [ "Cloneable", null, [
+      [ "Castling", "class_castling.html", null ],
+      [ "Chess", "class_chess.html", null ],
+      [ "Movement", "class_movement.html", null ],
+      [ "Pair< S, T >", "class_pair.html", null ],
+      [ "Piece", "class_piece.html", null ],
+      [ "PieceType", "class_piece_type.html", null ],
+      [ "Position", "class_position.html", null ],
+      [ "Turn", "class_turn.html", null ]
+    ] ],
+    [ "ConsoleGame", "class_console_game.html", null ],
+    [ "Cpu", "class_cpu.html", null ],
+    [ "Exception", null, [
+      [ "JSONParseFormatException", "class_j_s_o_n_parse_format_exception.html", null ]
+    ] ],
+    [ "FromJSONParserHelper", "class_from_j_s_o_n_parser_helper.html", null ],
+    [ "GameController", "class_game_controller.html", null ],
+    [ "ItemBuilder", "class_item_builder.html", null ],
+    [ "JSON", "interface_j_s_o_n.html", [
+      [ "Castling", "class_castling.html", null ],
+      [ "Movement", "class_movement.html", null ],
+      [ "Piece", "class_piece.html", null ],
+      [ "PieceType", "class_piece_type.html", null ],
+      [ "Position", "class_position.html", null ],
+      [ "Turn", "class_turn.html", null ]
+    ] ],
+    [ "Knowledge", "class_knowledge.html", null ],
+    [ "MoveAction", "enum_move_action.html", null ],
+    [ "Pair< String, String >", "class_pair.html", null ],
+    [ "PieceColor", "enum_piece_color.html", null ],
+    [ "ToJSONParserHelper", "class_to_j_s_o_n_parser_helper.html", null ],
+    [ "Application", null, [
+      [ "UIChess", "class_u_i_chess.html", null ]
+    ] ],
+    [ "ImageView", null, [
+      [ "ImageTile", "class_image_tile.html", null ]
+    ] ],
+    [ "StackPane", null, [
+      [ "UIPiece", "class_u_i_piece.html", null ]
+    ] ]
+];
---
+++ doc/html/interface_j_s_o_n.js
@@ -0,0 +1,4 @@
+var interface_j_s_o_n =
+[
+    [ "toJSON", "interface_j_s_o_n.html#a6e34eb5903b6552258d5eeaec9e873bf", null ]
+];
